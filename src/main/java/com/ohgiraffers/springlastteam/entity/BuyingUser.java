850f7e44368b4646120e15e0f13b197a44795389
ieunseo <y949512@naver.com>
2024-06-04 19:30:50 +0900

    수정중

--- src/main/java/com/ohgiraffers/springlastteam/entity/BuyingUser.java
+++ src/main/java/com/ohgiraffers/springlastteam/entity/BuyingUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Table(name = "buying_user")
@@ -21,6 +22,10 @@ public class BuyingUser implements Serializable {
 
     @Column(name = "buying_quantity")
     private int buyingQuantity;
+    @Column
+
+    @Temporal(TemporalType.DATE)
+    private Date buyingDate;
 
     @MapsId("buyingNo")
     @ManyToOne
@@ -32,10 +37,11 @@ public class BuyingUser implements Serializable {
     @JoinColumn(name="user_no")
     private Users userNo;
 
-    public BuyingUser(BuyingUserId id, int buyingPerson, int buyingQuantity, GroupBuying buyingNo, Users userNo) {
+    public BuyingUser(BuyingUserId id, int buyingPerson, int buyingQuantity, Date buyingDate, GroupBuying buyingNo, Users userNo) {
         this.id = id;
         this.buyingPerson = buyingPerson;
         this.buyingQuantity = buyingQuantity;
+        this.buyingDate = buyingDate;
         this.buyingNo = buyingNo;
         this.userNo = userNo;
     }
--- src/main/java/com/ohgiraffers/springlastteam/mypage/controller/MypageController.java
+++ src/main/java/com/ohgiraffers/springlastteam/mypage/controller/MypageController.java
@@ -1,7 +1,13 @@
 package com.ohgiraffers.springlastteam.mypage.controller;
 
+import com.ohgiraffers.springlastteam.entity.BuyingUser;
+import com.ohgiraffers.springlastteam.entity.GroupBuying;
+import com.ohgiraffers.springlastteam.entity.Users;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.*;
 
 @Controller
 public class MypageController {
@@ -10,4 +16,33 @@ public class MypageController {
     public String mypage() {
         return "mypage/mypage";
     }
+
+    @GetMapping("/purchashistory")
+    public String getPurchaseHistory(Model model) {
+        // 예시 판매자 생성
+        Users seller = new Users();
+        seller.setUserNo(2);
+        seller.setUserName("판매자");
+        // 예시 GroupBuying 객체 생성 (판매자 정보 포함)
+
+        GroupBuying groupBuying = new GroupBuying();
+        groupBuying.setBuyingNo(1);
+        groupBuying.setBuyingTitle("농산품 > 고구마");
+        groupBuying.setUser(seller);
+
+        // 예시 BuyingUser 객체 생성 (구매 정보 포함)
+        BuyingUser transaction = new BuyingUser();
+        transaction.setBuyingNo(groupBuying);
+        transaction.setBuyingDate(new Date());
+        transaction.setBuyingQuantity(3);
+        transaction.setBuyingPerson(1);
+
+        List<BuyingUser> transactions = new ArrayList<>();
+        transactions.add(transaction);
+
+        model.addAttribute("profileName", "김애란"); // 프로필 이름 직접 설정
+        model.addAttribute("transactions", transactions);
+
+        return "mypage/purchashistory";
+        }
 }
